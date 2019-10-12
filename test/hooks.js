ee8a16e97c600c254c6c316ec4702a42ff5af183
Diego Paez <paez.di@gmail.com>
2019-10-12 13:15:29 -0300

    Update: add promise based tests using original test base

--- test/hooks.js
+++ test/hooks.js
@@ -37,6 +37,40 @@ tape('test level-hookdown', function (t) {
   }
 })
 
+tape('[promises] test level-hookdown', async function (t) {
+  var sublevel = sub(MemDB(), 'test', { valueEncoding: 'json' })
+  var db = hook(sublevel)
+
+  t.plan(4)
+
+  db.prehooks.push(prehook)
+  db.posthooks.push(posthook)
+
+  var setVal = {
+    beep: 'boop'
+  }
+
+  t.doesNotThrow(async function () {
+    await db.put('main', setVal)
+  }, 'put on wrapped db is error free')
+
+  function prehook (operation, cb) {
+    db.get('main', function (err, value) {
+      t.equal(err.type, 'NotFoundError', 'main operation hasnt been performed in prehook')
+      cb(err.type !== 'NotFoundError' ? err : null)
+    })
+  }
+
+  function posthook (operation, cb) {
+    db.get('main', function (err, value) {
+      t.error(err)
+      if (err) return cb(err)
+      t.deepEqual(value, setVal, 'main put worked fine')
+      cb()
+    })
+  }
+})
+
 tape('test level-hookdown series', function (t) {
   var level = MemDB({ valueEncoding: 'json' })
   var db = hook(level, { type: 'series' })
@@ -71,6 +105,40 @@ tape('test level-hookdown series', function (t) {
   }
 })
 
+tape('[promises] test level-hookdown series', function (t) {
+  var level = MemDB({ valueEncoding: 'json' })
+  var db = hook(level, { type: 'series' })
+
+  t.plan(4)
+
+  db.prehooks.push(prehook)
+  db.posthooks.push(posthook)
+
+  var setVal = {
+    beep: 'boop'
+  }
+
+  t.doesNotThrow(async function () {
+    await db.put('main', setVal)
+  }, 'put on wrapped db is error free')
+
+  function prehook (operation, cb) {
+    db.get('main', function (err, value) {
+      t.equal(err.type, 'NotFoundError', 'main operation hasnt been performed in prehook')
+      cb(err.type !== 'NotFoundError' ? err : null)
+    })
+  }
+
+  function posthook (operation, cb) {
+    db.get('main', function (err, value) {
+      t.error(err)
+      if (err) return cb(err)
+      t.deepEqual(value, setVal, 'main put worked fine')
+      cb()
+    })
+  }
+})
+
 tape('test level-hookdown limit', function (t) {
   var sublevel = sub(MemDB(), 'test', { valueEncoding: 'json' })
   var db = hook(sublevel, { type: 'limit', limit: 2 })
@@ -87,6 +155,40 @@ tape('test level-hookdown limit', function (t) {
   db.put('main', setVal, function (err) {
     t.error(err, 'put on wrapped db is error free')
   })
+
+  function prehook (operation, cb) {
+    db.get('main', function (err, value) {
+      t.equal(err.type, 'NotFoundError', 'main operation hasnt been performed in prehook')
+      cb(err.type !== 'NotFoundError' ? err : null)
+    })
+  }
+
+  function posthook (operation, cb) {
+    db.get('main', function (err, value) {
+      t.error(err)
+      if (err) return cb(err)
+      t.deepEqual(value, setVal, 'main put worked fine')
+      setTimeout(cb, 100)
+    })
+  }
+})
+
+tape('[promises] test level-hookdown limit', function (t) {
+  var sublevel = sub(MemDB(), 'test', { valueEncoding: 'json' })
+  var db = hook(sublevel, { type: 'limit', limit: 2 })
+
+  t.plan(4)
+
+  db.prehooks.push(prehook)
+  db.posthooks.push(posthook)
+
+  var setVal = {
+    beep: 'boop'
+  }
+
+  t.doesNotThrow(async function () {
+    await db.put('main', setVal)
+  }, 'put on wrapped db is error free')
 
   function prehook (operation, cb) {
     db.get('main', function (err, value) {
